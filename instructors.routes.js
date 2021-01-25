a2da24185c5595224aa61965b038a9e216965f74
Rafael Ramires <moreira.ramires@gmail.com>
2021-01-25 20:07:57 -0300

    Configurando datas universais

--- instructors.routes.js
+++ instructors.routes.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const data = require('./data.json')
-const {age} = require("./uteis")
+const {age, date} = require("./uteis")
 
 // ===*Show*=== 
 
@@ -76,9 +76,13 @@ exports.edit = function(req,res){
   })
   if( !foundInstructor) return res.send("Instructor not found")
  
-  
+    const instructor ={
+      ...foundInstructor,
+      birth: date(foundInstructor.birth)
+    }
 
-  return res.render('instructors/edit', {instructor:foundInstructor})
+
+  return res.render('instructors/edit', {instructor})
 }
 // ===*Delete*===
 
--- uteis.js
+++ uteis.js
@@ -3,13 +3,21 @@ module.exports={
     let today = new Date()
     const birthDate = new Date ( timestamp)
   
-    let age = today.getFullYear()- birthDate.getFullYear()
-    const month = today.getMonth() - birthDate.getMonth()
+    let age = today.getUTCFullYear()- birthDate.getUTCFullYear()
+    const month = today.getUTCMonth() - birthDate.getUTCMonth()
   
-    if ( month < 0 || month == 0 && today.getDate() < birthDate.getDate() ){
+    if ( month < 0 || month == 0 && today.getUTCDate() < birthDate.getUTCDate() ){
       age = age -1
     }
   
     return age
+  },
+  date: function(timestamp){
+    const date = new Date(timestamp);
+    const year = date.getUTCFullYear();
+    const month = date.getUTCMonth() +1 ;
+    const day = date.getUTCDate();
+
+    return `${year}/ ${month}/${day} 📅 `
   }
 }
